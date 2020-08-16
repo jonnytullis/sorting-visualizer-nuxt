7dfacec4c4ed1db93f8e2c41a5e4fc5291dcf2af
Jonny Tullis <jon.tullis558@gmail.com>
2020-08-16 14:44:05 -0600

    Added bubble sort skeleton

--- assets/algorithms/BubbleSort.js
+++ assets/algorithms/BubbleSort.js
@@ -2,19 +2,20 @@ import Sort from "./Sort";
 
 export default class BubbleSort extends Sort {
   static colors = {
-    pivot: 'red',
-    currentIteration: 'grey',
+    parentNode: 'red',
+    leftChild: 'orange',
+    rightChild: 'purple',
     swapping: 'yellow',
-    lessThanPivot: 'purple',
+    rootNode: 'grey',
     sorted: 'success'
   }
 
   async sort () {
-    await this.bubbleSort(0, this.arr.length - 1)
+    await this.bubbleSort()
     return new Promise(resolve => resolve())
   }
 
-  async bubbleSort (low, high) {
+  async bubbleSort () {
   }
 }
 
